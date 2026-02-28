d7e1690dc7809f9f47067e9cd96cc44ca034741d
kotvpalto <kotvpaltoof@ya.ru>
2026-02-28 17:55:26 +0300

    gui гланвй экран

--- gui/app.py
+++ gui/app.py
@@ -1,9 +1,55 @@
 from tkinter import PhotoImage
 import customtkinter as ctk
 from PIL import Image
+from utils import sorting
 
 # ctk.set_appearance_mode("Dark")
 # ctk.set_default_color_theme("blue")
+
+def label_image(master: any, path: str, size: tuple[int, int], rotate: int = 0) -> ctk.CTkLabel:
+    img = Image.open(path).convert("RGBA")
+    img = img.rotate(rotate)
+    img = img.resize(size, Image.Resampling.LANCZOS)
+    
+    ctk_image = ctk.CTkImage(light_image=img, dark_image=img, size=size)
+    
+    label = ctk.CTkLabel(master=master, text="", image=ctk_image, fg_color="transparent")
+    label.image = ctk_image
+    
+    return label
+
+class WarningPopup(ctk.CTkToplevel):
+    def __init__(self, parent, warn_text):
+        super().__init__(parent)
+        
+        self.title("Warning!")
+        self.geometry("300x1")
+        self.resizable(False, False)
+
+        self.transient(parent)
+        self.grab_set()
+
+        self.configure(fg_color="#836F00")
+        
+        inner_frame = ctk.CTkFrame(self, fg_color=ctk.ThemeManager.theme["CTk"]["fg_color"])
+        inner_frame.pack(padx=5, pady=5, fill="both", expand=True)
+
+        self.label = ctk.CTkLabel(inner_frame, text=warn_text, wraplength=280, justify="center")
+        self.label.pack(pady=10, padx=20)
+        
+        self.button = ctk.CTkButton(inner_frame, text="OK", command=self.destroy, width=80)
+        self.button.pack(pady=(0, 10), padx=10)
+
+        self.update_idletasks() 
+
+        width = self.winfo_reqwidth()   
+        height = self.winfo_reqheight()
+        
+        self.geometry(f"{width}x{height}") 
+        
+        x = parent.winfo_x() + (parent.winfo_width() - width) // 2
+        y = parent.winfo_y() + (parent.winfo_height() - height) // 2
+        self.geometry(f"+{x}+{y}")
 
 class DobotApp(ctk.CTk):
     def __init__(self):
@@ -11,7 +57,7 @@ class DobotApp(ctk.CTk):
 
         self.title("dobot-hell-2026")
         self.geometry("900x600")
-        self.resizable(False, False)
+        # self.resizable(False, False)
 
         # self.after(100, self.insert_icon) TODO: fix this shit
 
@@ -58,11 +104,43 @@ class DobotApp(ctk.CTk):
         self.camera_page()
         self.settings_page()
 
+    def visual_zone(self, parent):
+        frame = ctk.CTkFrame(master=parent, width=900, height=600, fg_color="transparent")
+        frame.pack_propagate(False)
+
+        base_path = "./gui/images"
+
+        base_dobot = label_image(frame, f"{base_path}/dobot/offline.png", (128, 100))
+        base_dobot.place(x=25, y=100)
+
+        help_dobot = label_image(frame, f"{base_path}/dobot/offline.png", (128, 100), 90)
+        help_dobot.place(x=125, y=10)
+
+        return frame
+
     def home_page(self):
         frame_name = "main"
         frame = ctk.CTkFrame(self.main_area)
         
         ctk.CTkLabel(frame, text=f"well cum to {frame_name}", font=ctk.CTkFont(size=24)).pack(pady=20)
+
+        # visual = self.visual_zone(parent=frame)
+        # visual.pack()
+
+        def start_button_event():
+            cubes = start_cubes.get()
+            try:
+                cubes = int(cubes)
+            except:
+                popup = WarningPopup(app, "Введите целое кол-во кубиков!")
+                return
+            sorting.start_sorting(cubes=cubes)
+    
+        start_button = ctk.CTkButton(frame, command=start_button_event, text="СТАРТ!")
+        start_cubes = ctk.CTkEntry(frame, placeholder_text="колво кубов")  
+
+        start_button.pack()
+        start_cubes.pack()
 
         self.frames[frame_name] = frame
         self.sidebar_pages.append((frame_name, "main"))
